6101f6149401a2c2a7283f8d0092cc029dc8dd4e
Paweł Zmarzły <pawo2500@gmail.com>
2018-07-23 18:36:00 +0200

    add binary building script

---
+++ build-binary-release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf tmp
+mkdir tmp
+mkdir tmp/x11-input-mirror
+cargo build --release
+cp target/release/grabber tmp/x11-input-mirror/
+cp target/release/server tmp/x11-input-mirror/
+cp GrabberConfig.toml tmp/x11-input-mirror/
+cp SecurityConfig.toml tmp/x11-input-mirror/
+cp ServerConfig.toml tmp/x11-input-mirror/
+mkdir tmp/x11-input-mirror/Xephyr
+cp Xephyr/i3-chrome tmp/x11-input-mirror/Xephyr/
+cp Xephyr/start.sh tmp/x11-input-mirror/Xephyr/
+cd tmp
+7z a -tzip -mx1 x11-input-mirror.zip x11-input-mirror
+cd ..
+mv tmp/x11-input-mirror.zip .
+rm -rf tmp
